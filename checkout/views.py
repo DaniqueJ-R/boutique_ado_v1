a33f65044eea521fe2412b61f037b4787ffef94e
DaniqueJ-R <24005981@student.peterborough.ac.uk>
2025-09-30 20:39:25 +0100

    added stripe element

--- checkout/views.py
+++ checkout/views.py
@@ -13,7 +13,9 @@ def checkout(request):
     order_form = OrderForm()
     template = 'checkout/checkout.html'
     context = {
-        'order_form': order_form
+        'order_form': order_form,
+        'stripe_public_key': 'pk_test_51SD9GfBmYOT5xLk0Ykr5gAbnu8PcxOCxXNn7dwh2ZRR61brPPDJ4kVOrbkX0JbKqCUkXfI1FSZCMt0fM0K9eJU8r00316FuTcu',
+        'client_secret': 'test client secret',
     }
 
     return render(request, template, context)
